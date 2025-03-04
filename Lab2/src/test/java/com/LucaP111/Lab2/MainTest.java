4e291c1a7800a27acbe3d7755ee0f9db3ff440d3
Pete Rock <lucapopoviciu@yahoo.com>
2025-03-04 16:59:10 +0200

    Updated Tests Lab2

--- Lab2/src/test/java/com/LucaP111/Lab2/MainTest.java
+++ Lab2/src/test/java/com/LucaP111/Lab2/MainTest.java
@@ -1,22 +1,22 @@
 package com.LucaP111.Lab2;
 
-import com.google.common.base.Joiner;
+import com.google.common.base.Splitter;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
     @Test
-    void testConcatenare() {
-        String rezultat = Joiner.on(",").join("Walk" , "to" , "store");
-        assertEquals(rezultat, "Walk,to,store");
+    void testSplitString() {
+        List<String> rezultat = Splitter.on(",").splitToList("Walk,to,store");
+        assertEquals(List.of("Walk", "to", "store"), rezultat);
     }
 
     @Test
-    void testStringGol() {
-        String rezultat = Joiner.on("").join("" , "" , "");
-        assertEquals(rezultat, "");
+    void testSplitStringWithEmptyElements() {
+        List<String> rezultat = Splitter.on(",").omitEmptyStrings().splitToList("Walk,,to,store");
+        assertEquals(List.of("Walk", "to", "store"), rezultat);
     }
 }
-
-;

ea194c3cddb91ce5c95328d0fdba8e9a186dc6f1
Pete Rock <lucapopoviciu@yahoo.com>
2025-03-04 16:24:43 +0200

    Changed Tests Lab2

--- Lab2/src/test/java/com/LucaP111/Lab2/MainTest.java
+++ Lab2/src/test/java/com/LucaP111/Lab2/MainTest.java
@@ -1,20 +1,22 @@
 package com.LucaP111.Lab2;
 
-
-
+import com.google.common.base.Joiner;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
     @Test
     void testConcatenare() {
-        String rezultat = "Walk, to, store";
-        assertEquals(rezultat, "Walk, to, store");
+        String rezultat = Joiner.on(",").join("Walk" , "to" , "store");
+        assertEquals(rezultat, "Walk,to,store");
     }
 
     @Test
     void testStringGol() {
-        String rezultat = "";
+        String rezultat = Joiner.on("").join("" , "" , "");
         assertEquals(rezultat, "");
     }
 }
+
+;

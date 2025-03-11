a5fe2e8e945f8eeae4cce7392cd6abcfbf81c15d
Pete Rock <lucapopoviciu@yahoo.com>
2025-03-11 17:33:05 +0200

    Updated Tests Lab2

--- Lab2/src/main/java/com/LucaP111/Lab2/Main.java
+++ Lab2/src/main/java/com/LucaP111/Lab2/Main.java
@@ -1,12 +1,15 @@
 package com.LucaP111.Lab2;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-import com.google.common.base.Joiner;
+import com.google.common.base.Splitter;
+
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        String rezultat = Joiner.on(", ").join("Walk", "to", "store");
-        System.out.println(rezultat);
+        String input = "Walk, to, store";
+
+        List<String> splitResult = Splitter.on(", ").splitToList(input);
+
+        System.out.println("Split string: " + splitResult);
     }
 }
